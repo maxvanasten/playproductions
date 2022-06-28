8c23fba00ac19e0419ba851591212c490534a186
Max van Asten <maxvasten@gmail.com>
2022-06-28 17:55:24 +0200

    Possible path fix for ejs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 // Connect to mysql database
 const databaseCredentials = require("./configuration/database.json");
