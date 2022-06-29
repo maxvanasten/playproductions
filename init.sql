9f963b39dcaae12be03bacbe8aea89b875a68906
Max van Asten <maxvasten@gmail.com>
2022-06-29 20:24:16 +0200

    Update database design

--- init.sql
+++ init.sql
@@ -2,28 +2,29 @@ CREATE DATABASE IF NOT EXISTS playproductions;
 
 USE playproductions;
 
-CREATE TABLE IF NOT EXISTS `orders` (
-    orderDate DATETIME, -- date the order was placed
-    products JSON, -- a list of products and their properties that were ordered
-    total FLOAT -- the total price of this order
+CREATE TABLE IF NOT EXISTS `products` (
+    reference VARCHAR(20),
+    name VARCHAR(32),
+    description VARCHAR(256),
+    price FLOAT,
+    sales INT
 );
 
 CREATE TABLE IF NOT EXISTS `bookings` (
-    startTime DATETIME,
-    endTime DATETIME,
-    contact VARCHAR(50)
+    reference VARCHAR(20),
+    phoneNumber VARCHAR(10),
+    emailAddress VARCHAR(48),
+    address VARCHAR(60),
+    type STR,
+    deposit FLOAT,
+    date DATETIME
 );
 
--- CREATE TABLE IF NOT EXISTS `reviews` (
-
--- );
-
--- CREATE TABLE IF NOT EXISTS `posts` (
-
--- );
-
-CREATE TABLE IF NOT EXISTS `products` (
-    productName VARCHAR(50),
-    productDesc VARCHAR(255),
-    price FLOAT
+CREATE TABLE IF NOT EXISTS `orders` (
+    reference VARCHAR(20),
+    products JSON,
+    date DATETIME,
+    total FLOAT,
+    bookingReference VARCHAR(20)
 );
+
